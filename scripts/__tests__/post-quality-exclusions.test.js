7c991f67b60fbfa1ae3522db050f4d390fa6ab8c
sazeka <sazeka@gmail.com>
2026-09-22 11:39:55 -0700

    Reduce appointment-track unknowns to 1084

--- scripts/__tests__/post-quality-exclusions.test.js
+++ scripts/__tests__/post-quality-exclusions.test.js
@@ -27,6 +27,11 @@ test('reviewed false listings are excluded only by their exact URLs', () => {
     { url: 'https://www.callutheran.edu/academics/faculty-mentorship.html', reason: 'student_program_information_page' },
     { url: 'https://careers.usi.edu/jobs/search?page=1&employment_type_uids%5B%5D=2870671dfb7b1b2953f9187844ba74a2&query=', reason: 'generic_job_search_page' },
     { url: 'https://www.uhsp.edu/academics/st-louis-college-of-pharmacy/dean-of-pharmacy', reason: 'faculty_profile_or_leadership_page' },
+    { url: 'https://today.oregonstate.edu/all-stories/10-questions-diana-rohlman-associate-professor-and-senior-researcher-department', reason: 'news_or_profile_page' },
+    { url: 'https://www.uvmhealth.org/medical-education-training/champlain-valley-physicians-hospital-residency-education/school-of-radiologic-technology/faculty-board-members', reason: 'faculty_staff_page' },
+    { url: 'https://saltlakecommunitycollege.blogspot.com/2022/03/meet-our-faculty-dr-emmanuel-santa.html', reason: 'news_or_profile_page' },
+    { url: 'https://www.fhnw.edu/staff/department/gove-county-faculty', reason: 'faculty_staff_page' },
+    { url: 'https://www.asl.edu/faculty-2/faculty-assistant', reason: 'administrative_staff_profile' },
   ])
   assert.equal(reviewedExclusionReason({ url: 'https://www.albanylaw.edu/faculty-community/' }, exclusions), 'news_or_profile_page')
   assert.equal(reviewedExclusionReason({ url: 'https://www.albanylaw.edu/about/employment' }, exclusions), null)
@@ -34,4 +39,10 @@ test('reviewed false listings are excluded only by their exact URLs', () => {
   assert.equal(reviewedExclusionReason({ url: 'https://www.callutheran.edu/academics/faculty-mentorship.html' }, exclusions), 'student_program_information_page')
   assert.equal(reviewedExclusionReason({ url: 'https://careers.usi.edu/jobs/search?page=1&employment_type_uids%5B%5D=2870671dfb7b1b2953f9187844ba74a2&query=' }, exclusions), 'generic_job_search_page')
   assert.equal(reviewedExclusionReason({ url: 'https://www.uhsp.edu/academics/st-louis-college-of-pharmacy/dean-of-pharmacy/' }, exclusions), 'faculty_profile_or_leadership_page')
+  assert.equal(reviewedExclusionReason({ url: 'https://today.oregonstate.edu/all-stories/10-questions-diana-rohlman-associate-professor-and-senior-researcher-department/' }, exclusions), 'news_or_profile_page')
+  assert.equal(reviewedExclusionReason({ url: 'https://www.uvmhealth.org/medical-education-training/champlain-valley-physicians-hospital-residency-education/school-of-radiologic-technology/faculty-board-members' }, exclusions), 'faculty_staff_page')
+  assert.equal(reviewedExclusionReason({ url: 'https://saltlakecommunitycollege.blogspot.com/2022/03/meet-our-faculty-dr-emmanuel-santa.html' }, exclusions), 'news_or_profile_page')
+  assert.equal(reviewedExclusionReason({ url: 'https://www.fhnw.edu/staff/department/gove-county-faculty' }, exclusions), 'faculty_staff_page')
+  assert.equal(reviewedExclusionReason({ url: 'https://www.asl.edu/faculty-2/faculty-assistant/' }, exclusions), 'administrative_staff_profile')
+  assert.equal(reviewedExclusionReason({ url: 'https://www.asl.edu/about/employment' }, exclusions), null)
 })
